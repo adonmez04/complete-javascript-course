411bfbb6675af6cd3e4439090df07e998361663f
alper <adonmez0417@gmail.com>
2024-07-13 15:06:12 +0300

    The coding challenge 3 was finished

--- 01-Fundamentals-Part-1/starter/solutions.js
+++ 01-Fundamentals-Part-1/starter/solutions.js
@@ -27,17 +27,17 @@ GOOD LUCK 😀
 
 // TEST DATA 1: Marks weights 78 kg and is 1.69 m tall.
 //   John weights 92 kg and is 1.95 m tall.
-// let marksMass = 78;
-// let marksHeight = 1.69
-// let johnMass = 92;
-// let johnHeight = 1.95;
+let marksMass = 78;
+let marksHeight = 1.69
+let johnMass = 92;
+let johnHeight = 1.95;
 
 // TEST DATA 2: Marks weights 95 kg and is 1.88 m tall.
 //   John weights 85 kg and is 1.76 m tall.
-let marksMass = 95;
-let marksHeight = 1.88
-let johnMass = 85;
-let johnHeight = 1.76;
+// let marksMass = 95;
+// let marksHeight = 1.88
+// let johnMass = 85;
+// let johnHeight = 1.76;
 
 // their BMI (Body Mass Index), which is calculated using the formula:
 // BMI = mass / height ** 2 = mass / (height * height). (mass in kg and height in meter).
@@ -96,8 +96,85 @@ GOOD LUCK 😀
 // **************************************************************************
 // My Solution
 
-if (markBMI > johnBMI) {
-  console.log(`Marks' BMI (${parseInt(markBMI)}) is higher than John's BMI (${parseInt(johnBMI)})...`);
-} else {
-  console.log(`John's BMI (${parseInt(johnBMI)}) is higher than Marks' BMI (${parseInt(markBMI)})...`);
+// if (markBMI > johnBMI) {
+//   console.log(`Marks' BMI (${parseInt(markBMI)}) is higher than John's BMI (${parseInt(johnBMI)})...`);
+// } else {
+//   console.log(`John's BMI (${parseInt(johnBMI)}) is higher than Marks' BMI (${parseInt(markBMI)})...`);
+// }
+
+// **************************************************************************
+// Coding Challenge #3
+// **************************************************************************
+
+/*
+There are two gymnastics teams, Dolphins and Koalas.
+They compete against each other 3 times.
+The winner with the highest average score wins the trophy!
+
+1. Calculate the average score for each team, using the test data below
+
+2. Compare the team's average scores to determine the winner of the competition,
+and print it to the console. Don't forget that there can be a draw,
+so test for that as well (draw means they have the same average score).
+
+3. BONUS 1: Include a requirement for a minimum score of 100.
+With this rule, a team only wins if it has a higher score than the other team,
+and the same time a score of at least 100 points.
+HINT: Use a logical operator to test for minimum score,
+as well as multiple else-if blocks 😉
+
+4. BONUS 2: Minimum score also applies to a draw!
+So a draw only happens when both teams have the same score and
+both have a score greater or equal 100 points.
+Otherwise, no team wins the trophy.
+
+TEST DATA:
+Dolphins score 96, 108 and 89.
+Koalas score 88, 91 and 110
+
+TEST DATA BONUS 1:
+Dolphins score 97, 112 and 101.
+Koalas score 109, 95 and 123
+
+TEST DATA BONUS 2:
+Dolphins score 97, 112 and 101.
+Koalas score 109, 95 and 106
+
+GOOD LUCK 😀
+*/
+
+function takeAverage(x, y, z){
+  let average;
+  average = (x+y+z) / 3;
+  return average.toFixed(0);
 }
+
+function checkWin (team1, team2){
+  if ((team1 || team2) >= 100){
+    if (team1 > team2){
+      console.log("Dolphins win the trophy!")
+    }
+    else if (team2 > team1){
+      console.log("Koalas win the trophy!")
+    }
+    else if (team1 === team2){
+      console.log("Draw!")
+    }
+  } else {
+    console.log("No team wins the trophy.");
+  }
+}
+
+// TEST DATA: Dolphins score 96, 108 and 89. Koalas score 88, 91 and 110
+// TEST DATA BONUS 1: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 123
+// TEST DATA BONUS 2: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 106
+
+checkWin (takeAverage(97, 121, 101), takeAverage(109, 95, 123));
+
+
+
+
+
+
+
+
