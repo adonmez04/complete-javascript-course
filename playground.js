2a7917953de00327fe0eb321b9c5e972d295f62c
alper <adonmez0417@gmail.com>
2024-07-18 16:05:05 +0300

    the fundamental part 2 was added

---
+++ playground.js
@@ -0,0 +1,13 @@
+// 'use strict'
+//
+// let hasDriversLicense = false;
+// const passTest = true;
+//
+// if (passTest) hasDriversLicense = true;
+// if (hasDriversLicense) console.log('I can drive!');
+
+function logger (){
+  return 'My name is Jonas';
+}
+logger();
+// console.log(logger());
